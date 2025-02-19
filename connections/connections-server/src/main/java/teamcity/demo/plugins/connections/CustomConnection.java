46a4ad9a3d1567bd889b9a323fc6df6060354801
Andrey Titov <andrey.titov@jetbrains.com>
2025-02-19 13:46:42 +0100

    Add files needed to provide a new connection type "Custom Connection": - class CustomConnection (extends OAuthProvider): to describe the connection (type, name, default properties, JSP location) - customConnectionEditForm.jsp: contains minimal set of fields to save username and password in the connection
    The OAuthProvider extension should be added to the Spring context in order TeamCity Core can find it.

---
+++ connections/connections-server/src/main/java/teamcity/demo/plugins/connections/CustomConnection.java
@@ -0,0 +1,73 @@
+package teamcity.demo.plugins.connections;
+
+import jetbrains.buildServer.serverSide.InvalidProperty;
+import jetbrains.buildServer.serverSide.PropertiesProcessor;
+import jetbrains.buildServer.serverSide.oauth.OAuthProvider;
+import jetbrains.buildServer.web.openapi.PluginDescriptor;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+
+/**
+ * OAuthProvider is the base class for project-level Connections
+ *
+ * The OAuthProvider extension should be added to the Spring context in order TeamCity Core
+ * can find it.
+ */
+public class CustomConnection extends OAuthProvider {
+
+    private final PluginDescriptor pluginDescriptor;
+
+    public CustomConnection(PluginDescriptor pluginDescriptor) {
+        this.pluginDescriptor = pluginDescriptor;
+    }
+
+    /**
+     * The properties processor can check the form during save action and raise errors
+     *
+     * @return
+     */
+    @Nullable
+    @Override
+    public PropertiesProcessor getPropertiesProcessor() {
+        return new PropertiesProcessor() {
+            @Override
+            public Collection<InvalidProperty> process(Map<String, String> map) {
+                return Collections.emptyList();
+            }
+        };
+    }
+
+    @NotNull
+    @Override
+    public String getType() {
+        return "CustomConnection";
+    }
+
+    @Nullable
+    @Override
+    public Map<String, String> getDefaultProperties() {
+        return Collections.emptyMap();
+    }
+
+    @NotNull
+    @Override
+    public String getDisplayName() {
+        return "Custom Connection";
+    }
+
+    @Nullable
+    @Override
+    public String getEditParametersUrl() {
+        // PluginDescriptor.getPluginResourcesPath resolves the real location of the JSP after it's unpacked from the plugin
+        return this.pluginDescriptor.getPluginResourcesPath("customConnectionEditForm.jsp");
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return true;
+    }
+}
---
+++ connections/connections-server/src/main/resources/buildServerResources/customConnectionEditForm.jsp
@@ -0,0 +1,31 @@
+<%-- this JSP is inlined to a <table> containing Save and Cancel buttons below the form --%>
+
+<%-- can be used for all JSPs - includes many useful taglibs --%>
+<%@ include file="/include-internal.jsp" %>
+<%-- taglibs needed for the form --%>
+<%@ taglib prefix="forms" tagdir="/WEB-INF/tags/forms" %>
+<%@ taglib prefix="oauth" tagdir="/WEB-INF/tags/oauth" %>
+
+<%-- renders a required field "Display name" --%>
+<oauth:displayName note="Name this connection to distinguish it from others."/>
+
+<%-- renders username --%>
+<tr>
+    <th><label for="user">Username:<l:star/></label></th>
+    <td>
+        <props:textProperty name="user" className="longField"/>
+        <%-- TeamCity will put a validation errors from OAuthProvider.getPropertiesProcessor() here --%>
+        <span class="error" id="error_user"></span>
+        <span class="smallNote">Username</span>
+    </td>
+</tr>
+
+<tr>
+    <th><label for="secure:passwd">Password:<l:star/></label></th>
+    <td>
+        <%-- password properties are safely passed to backend, no additional efforts needed --%>
+        <props:passwordProperty name="secure:passwd" className="longField"/>
+        <%-- TeamCity will put a validation errors from OAuthProvider.getPropertiesProcessor() here --%>
+        <span class="error" id="error_secure:passwd"></span>
+    </td>
+</tr>
