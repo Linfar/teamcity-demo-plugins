8ec219e19a3d10d8b3d576055ea37268c788ac94
Andrey Titov <andrey.titov@jetbrains.com>
2025-03-14 17:32:38 +0100

    Implement test connection button

---
+++ connections/connections-server/src/main/java/teamcity/demo/plugins/connections/CustomConnectionCheckConnectionController.java
@@ -0,0 +1,97 @@
+package teamcity.demo.plugins.connections;
+
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
+import jetbrains.buildServer.controllers.*;
+import jetbrains.buildServer.controllers.admin.projects.PluginPropertiesUtil;
+import jetbrains.buildServer.http.SimpleCredentials;
+import jetbrains.buildServer.log.Loggers;
+import jetbrains.buildServer.serverSide.SBuildServer;
+import jetbrains.buildServer.util.HTTPRequestBuilder;
+import jetbrains.buildServer.util.ssl.SSLTrustStoreProvider;
+import jetbrains.buildServer.web.openapi.WebControllerManager;
+import org.jdom.Element;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class CustomConnectionCheckConnectionController extends BaseFormXmlController {
+    private final SSLTrustStoreProvider sslTrustStoreProvider;
+    private final HTTPRequestBuilder.RequestHandler requestHandler;
+    private Gson gson = new Gson();
+
+    public CustomConnectionCheckConnectionController(WebControllerManager webControllerManager,
+                                                     SSLTrustStoreProvider sslTrustStoreProvider,
+                                                     SBuildServer buildServer,
+                                                     HTTPRequestBuilder.RequestHandler requestHandler) {
+        super(buildServer);
+        this.sslTrustStoreProvider = sslTrustStoreProvider;
+        this.requestHandler = requestHandler;
+
+        webControllerManager.registerController("/repo/registry-test-connection.html", this);
+    }
+
+    @Override
+    protected ModelAndView doGet(@NotNull HttpServletRequest httpServletRequest, @NotNull HttpServletResponse httpServletResponse) {
+        return null;
+    }
+
+    @Override
+    protected void doPost(@NotNull HttpServletRequest httpServletRequest, @NotNull HttpServletResponse httpServletResponse, @NotNull Element xmlResponse) {
+        BasePropertiesBean propertiesBean = new BasePropertiesBean(Collections.emptyMap());
+        Map<String, String> properties = new HashMap<>();
+        if (!httpServletRequest.getParameterMap().isEmpty()) {
+            if (PublicKeyUtil.isPublicKeyExpired(httpServletRequest)) {
+                PublicKeyUtil.writePublicKeyExpiredError(xmlResponse);
+                return;
+            }
+            PluginPropertiesUtil.bindPropertiesFromRequest(httpServletRequest, propertiesBean);
+            properties.putAll(propertiesBean.getProperties());
+        }
+
+        bindPropertiesFromRequestBody(httpServletRequest, properties);
+
+        doTestConnection(properties, xmlResponse);
+    }
+
+    private void doTestConnection(Map<String, String> properties, Element xmlResponse) {
+        ActionErrors errors = new ActionErrors();
+
+        try {
+            HTTPRequestBuilder.Request request = HTTPRequestBuilder
+                    .request("https://theservice.com")
+                    .withAuthenticateHeader(new SimpleCredentials(properties.get(CustomConnection.USER), properties.get(CustomConnection.PASSWORD)))
+                    .withTrustStore(sslTrustStoreProvider.getTrustStore())
+                    .build();
+
+//            HTTPRequestBuilder.Response response = IOGuard.allowNetworkCall(() -> requestHandler.doSyncRequest(request));
+//            int statusCode = response.getStatusCode();
+
+//            if (statusCode == 200) {
+//                XmlResponseUtil.writeTestResult(xmlResponse, "Connection to theservice.com was successful!");
+//            } else {
+//                errors.addError("Error code", statusCode + " " + "response.getStatusText()");
+//            }
+
+            errors.addError("Error code", "The real test connection code is commented out");
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void bindPropertiesFromRequestBody(@NotNull HttpServletRequest httpServletRequest, Map<String, String> properties) {
+        TypeToken<Map<String, String>> mapType = new TypeToken<Map<String, String>>() {
+        };
+        try {
+            Map<String, String> jsonBody = gson.fromJson(httpServletRequest.getReader(), mapType.getType());
+            properties.putAll(jsonBody);
+        } catch (Exception ignored) {
+        }
+    }
+}
--- connections/connections-server/src/main/resources/buildServerResources/customConnectionEditForm.jsp
+++ connections/connections-server/src/main/resources/buildServerResources/customConnectionEditForm.jsp
@@ -10,6 +10,57 @@
 <oauth:displayName note="Name this connection to distinguish it from others."/>
 <%@ page import="teamcity.demo.plugins.connections.CustomConnection" %>
 
+<bs:linkScript>
+    /js/bs/testConnection.js
+</bs:linkScript>
+<script>
+    BS.OAuthConnectionDialog.submitTestConnection = function () {
+        var that = this;
+        BS.PasswordFormSaver.save(this, '<c:url value="/repo/registry-test-connection.html"/>', OO.extend(BS.ErrorsAwareListener, {
+            onFailedTestConnectionError: function (elem) {
+                var text = "";
+                if (elem.firstChild) {
+                    text = elem.firstChild.nodeValue;
+                }
+                BS.TestConnectionDialog.show(false, text, $('testConnectionButton'));
+            },
+            onCompleteSave: function (form, responseXML) {
+                var err = BS.XMLResponse.processErrors(responseXML, this, form.propertiesErrorsHandler);
+                BS.ErrorsAwareListener.onCompleteSave(form, responseXML, err);
+                if (!err) {
+                    this.onSuccessfulSave(responseXML);
+                }
+            },
+            onSuccessfulSave: function (responseXML) {
+                that.enable();
+                var additionalInfo = "";
+                var testConnectionResultNodes = responseXML.documentElement.getElementsByTagName("testConnectionResult");
+                if (testConnectionResultNodes && testConnectionResultNodes.length > 0) {
+                    var testConnectionResult = testConnectionResultNodes.item(0);
+                    if (testConnectionResult.firstChild) {
+                        additionalInfo = testConnectionResult.firstChild.nodeValue;
+                    }
+                }
+                BS.TestConnectionDialog.show(true, additionalInfo, $('testConnectionButton'));
+            }
+        }));
+        return false;
+    };
+
+    var afterClose = BS.OAuthConnectionDialog.afterClose;
+    BS.OAuthConnectionDialog.afterClose = function () {
+        $j('.testConnectionButton').remove();
+        afterClose()
+    };
+
+    var connectionTypeChanged = function(){
+        $j('#OAuthConnectionDialog .testConnectionButton').detach();
+        $j('#typeSelector').unbind('change', connectionTypeChanged);
+    };
+
+    $j('#typeSelector').on('change', connectionTypeChanged);
+</script>
+
 <%-- renders username --%>
 <tr>
     <th><label for="${CustomConnection.USER}">Username:<l:star/></label></th>
@@ -22,11 +73,22 @@
 </tr>
 
 <tr>
-    <th><label for="secure:${CustomConnection.PASSWORD}">Password:<l:star/></label></th>
+    <th><label for="${CustomConnection.PASSWORD}">Password:<l:star/></label></th>
     <td>
         <%-- password properties are safely passed to backend, no additional efforts needed --%>
-        <props:passwordProperty name="secure:${CustomConnection.PASSWORD}" className="longField"/>
+        <props:passwordProperty name="${CustomConnection.PASSWORD}" className="longField"/>
         <%-- TeamCity will put a validation error from OAuthProvider.getPropertiesProcessor() here --%>
-        <span class="error" id="error_secure:${CustomConnection.PASSWORD}"></span>
+        <span class="error" id="error_${CustomConnection.PASSWORD}"></span>
     </td>
 </tr>
+<forms:submit id="testConnectionButton" type="button" label="Test Connection" className="testConnectionButton"
+              onclick="return BS.OAuthConnectionDialog.submitTestConnection();"/>
+<bs:dialog dialogId="testConnectionDialog" title="Test Connection" closeCommand="BS.TestConnectionDialog.close();"
+           closeAttrs="showdiscardchangesmessage='false'">
+    <div id="testConnectionStatus"></div>
+    <div id="testConnectionDetails" class="mono"></div>
+</bs:dialog>
+<script>
+    $j('#OAuthConnectionDialog .popupSaveButtonsBlock .testConnectionButton').remove();
+    $j("#testConnectionButton").appendTo($j('#OAuthConnectionDialog .popupSaveButtonsBlock')[0])
+</script>
